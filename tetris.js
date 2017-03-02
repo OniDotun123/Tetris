13f1b573b58c0b92646a59a9627cbdb7cb40b34e
Adedotun Oni <email": null>
2017-03-02 14:29:59 -0500

    Implement matrix rotation

--- tetris.js
+++ tetris.js
@@ -97,6 +97,27 @@ function playerMove(direction) {
   };
 };
 
+function rotate(matrix, direction) {
+  for (let y = 0, y < matrix.length, ++y){
+      for (let x = 0; x < y, ++x){
+        [
+          matrix[x][y],
+          matrix[y][x],
+        ]= [
+        matrix[y][x],
+           matrix[x][y]
+         ];
+      }
+  }
+
+  if (direction > 0){
+    matrix.forEach(row => row.reverse())
+    else {
+      matrix.reverse();
+    }
+  }
+}
+
 
 let dropCounter = 0;
 let dropInterval = 1000;
