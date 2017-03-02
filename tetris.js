a720d46760364358e8f7d85e64e559f0c0e03865
Adedotun Oni <email": null>
2017-03-02 16:53:23 -0500

    create data structure that will represent each piece

--- tetris.js
+++ tetris.js
@@ -34,10 +34,46 @@ function createMatrix(w,h) {
 
 function createPiece(type) {
   if (type === "T") {
-    return   [
+    return [
       [0,0,0],
       [1,1,1],
       [0,1,0],
+    ];
+  }else if (type === "O") {
+    return [
+      [1,1],
+      [1,1],
+    ];
+  }else if (type === "L") {
+    return [
+      [0,1,0],
+      [0,1,0],
+      [0,1,1],
+    ];
+  }else if (type === "J") {
+    return [
+      [0,1,0],
+      [0,1,0],
+      [1,1,0],
+    ];
+  }else if (type === "I") {
+    return [
+      [0,1,0,0],
+      [0,1,0,0],
+      [0,1,0,0],
+      [0,1,0,0],
+    ];
+  }else if (type === "S") {
+    return [
+      [0,1,1],
+      [1,1,0],
+      [0,0,0],
+    ];
+  }else if (type === "Z") {
+    return [
+      [1,1,0],
+      [0,1,1],
+      [0,0,0],
     ];
   }
 }
@@ -100,6 +136,13 @@ function playerMove(direction) {
       player.pos.x -= direction;
   };
 };
+
+
+
+
+
+
+
 
 
 function playerRotate(direction) {
