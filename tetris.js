59994801081700c160156a9330e94e738952a5b0
Adedotun Oni <email": null>
2017-03-02 17:39:05 -0500

    Line gets cleared if there is a matching line

--- tetris.js
+++ tetris.js
@@ -3,7 +3,18 @@ const context = canvas.getContext("2d");
 
 context.scale(20,20);
 
-
+function arenaSweep() {
+  outer: for(let y = arena.length - 1; y > 0; --y){
+      for(let x = 0; x < arena[y].length; ++x){
+        if (arena[y][x] === 0 ){
+          continue outer;
+        }
+      }
+      const row = arena.splice(y,1)[0].fill(0);
+      arena.unshift(row);
+      ++y;
+    }
+}
 
 
 
@@ -125,6 +136,7 @@ function playerDrop() {
     player.pos.y--;
     merge(arena, player);
     playerReset();
+    arenaSweep();
   }
   dropCounter = 0;
 }
@@ -253,3 +265,4 @@ document.addEventListener('keydown', event => {
 
 
 update();
+console.log(arena)
